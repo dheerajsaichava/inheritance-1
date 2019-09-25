a5c1b5d6c5201b53cea86d28d0aa1448c5645bc5
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-24 22:21:55 -0400

    Add project files.

---
+++ inheritance 1/person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance_1
+{
+    class person
+    {
+    }
+}
---
+++ inheritance 1/player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance_1
+{
+    class player:person
+    {
+        int points;
+
+        player(int pts)
+
+        {
+
+            String FullName = FirstName + LastName;
+
+            pts = points;
+
+            Console.WriteLine("Employee " + FullName + " points are " + pts.ToString());
+
+        }
+
+        player()
+
+        {
+
+            String FullName = FirstName + LastName;
+
+            points = 846;
+
+            Console.WriteLine("player" + FullName + "points are" + points);
+
+        }
+
+    }
+
+
+
+}
+       
+    
+
